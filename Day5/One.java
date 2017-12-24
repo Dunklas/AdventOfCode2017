45f89d46fad3e9d10465ed96828fcfd34ea8b6fa
Rickard <dunklas@gmail.com>
2017-12-24 01:54:49 +0100

    Solved day 5

---
+++ Day5/One.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class One {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Missing filename");
+            return;
+        }
+
+        String filename = args[0];
+        int[] intInput = Files.lines(Paths.get(filename))
+            .map(i -> i.trim())
+            .mapToInt(i -> Integer.parseInt(i))
+            .toArray();
+
+        firstStar(Arrays.copyOf(intInput, intInput.length));
+        secondStar(Arrays.copyOf(intInput, intInput.length));
+    }
+
+    private static void firstStar(int[] instructions) {
+        
+        int numSteps = 0;
+        int index = 0;
+        while (index < instructions.length && index >= 0) {
+            int step = instructions[index]++;
+            index += step;
+            numSteps++;
+        }
+        System.out.println(numSteps);
+    }
+
+    private static void secondStar(int[] instructions) {
+
+        int numSteps = 0;
+        int index = 0;
+
+        while (index < instructions.length && index >= 0) {
+            int step = instructions[index];
+            instructions[index] += step >= 3 ? -1 : 1;
+            index += step;
+            numSteps++;
+        }
+        System.out.println(numSteps);
+    }
+}
